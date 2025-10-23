133d869e908e841b3eceea699dc8e98221d6b90d
EumSeungJae <tlwm3025@naver.com>
2025-10-23 17:58:21 +0900

    2025-10-23 6_2 함수정리
    결과도출 if문 헤더이동작업

--- SBSProgrammingF1/6_ 코드의 흐름 제어 과제/6_2main.c
+++ SBSProgrammingF1/6_ 코드의 흐름 제어 과제/6_2main.c
@@ -1,23 +1,5 @@
 
-//#include "ChoiceAndPoint.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h> // _getch() 함수 사용을 위해 필요
-#include <windows.h> // PlayerKeyBoardSelect() 함수 사용을 위해 필요
-#include <time.h>   // 랜덤 시드 설정을 위해 필요
-
-// 유저 값
-int playerchoice = 0;
-int playerpoint = 0;
-const char* playercharacter = "";
-
-// 컴퓨터 값
-int computerchoice = 0;
-int computerpoint = 0;
-const char* computercharacter = "";
-
-int Action = 0; // 결과갑 도출 1: 비김, 2: 플레이어 승리, 3: 컴퓨터 승리
+#include "ChoiceAndPoint.h"
 
 //  목표 : 가위바위보 구조 설계
 
@@ -33,92 +15,6 @@ int Action = 0; // 결과갑 도출 1: 비김, 2: 플레이어 승리, 3: 컴퓨터 승리
 *  7. 플레이어와 컴퓨터의 점수를 출력
 */
 
-void PlayerKeyBoardSelect()
-{
-	
-	while (1)
-	{
-
-
-		// 키 입력 처리
-		if (_kbhit()) 
-		{
-			int key = _getch();
-			if (key == 224) // 화살표 키 입력은 224가 먼저 입력됨
-			{ 
-				key = _getch();
-
-				if (key == 72)
-				{
-					playerchoice = 2;
-					playercharacter = "바위";
-					break;
-				}
-				else if (key == 75)
-				{
-					playerchoice = 1;
-					playercharacter = "가위";
-					break;
-				}
-				else if (key == 77)
-				{
-					playerchoice = 3;
-					playercharacter = "보";
-					break;
-
-				}
-			}
-		}
-
-	}
-}
-void ComputerRandomSelect()
-{
-	//stand(time(NULL));  // 컴퓨터의 랜덤 출력 방식
-	// 컴퓨터가 랜덤으로 선택한 결과를 저장
-	computerchoice = (rand() % 3) + 1; // 1~3 사이의 랜덤 숫자 생성
-	if (computerchoice == 1)
-	{
-		computercharacter = "가위";
-	}
-	else if (computerchoice == 2)
-	{
-		computercharacter = "바위";
-	}
-	else if (computerchoice == 3)
-	{
-		computercharacter = "보";
-	}
-	else
-		return;
-}
-void WinDecide()
-{
-
-	// 플레이어와 컴퓨터의 선택을 비교해서 승패를 결정
-	if (playerchoice == computerchoice)
-	{
-		// 비김
-
-		Action = 1;
-	}
-	else if ((playerchoice == 1 && computerchoice == 3) ||
-		(playerchoice == 2 && computerchoice == 1) ||
-		(playerchoice == 3 && computerchoice == 2))
-	{
-		// 플레이어 승리
-		playerpoint+1;
-
-		Action = 2;
-	}
-	else
-	{
-		// 컴퓨터 승리
-		computerpoint+1;
-
-		Action = 3;
-	}
-}
 int main()
 {
 	srand((unsigned int)time(NULL)); // 랜덤 시드
@@ -144,44 +40,6 @@ int main()
 		ComputerRandomSelect();
 		WinDecide();
 
-		printf("%s\n", playercharacter);
-
-
-		printf("  ====================== \n");
-		printf("플레이어의 선택 : %s  /  컴퓨터의 선택 :  %s\n", playercharacter, computercharacter);
-		
-		if (Action == 1)
-		{
-			printf("비겼습니다.\n");
-		}
-		else if (Action == 2)
-		{
-			playerpoint ++;
-			printf("플레이어가 승리했습니다.\n");
-		}
-		else if (Action == 3)
-		{
-			computerpoint ++;
-			printf("컴퓨터가 승리했습니다.\n");
-		}
-		printf("점수수코어 - 플레이어 %d : %d 컴퓨터\n", playerpoint, computerpoint);
-
-		printf("  ====================== \n");
-
-		if (playerpoint < 3 && computerpoint < 3)
-		{
-			printf("선택 :  ");
-		}
-		else if (playerpoint == 3)
-		{
-			printf(" --- 플레이어의 승리 ---");
-		}
-		else if (computerpoint == 3)
-		{
-			printf(" --- 컴퓨터의 승리 ---");
-		}
-		// _getch(); // 결과 확인 후 진행
-
 		//while(true)
 		//{
 		//    if(  컴퓨터 또는 유저가 목표 점수에 도달했을때
@@ -203,5 +61,6 @@ int main()
 *  여러분들만의 가위 바위 보 게임을 구현해보세요.
 * 
 *  //stand(time(NULL));  함수 사용으로 버그발생
+*  // 결과도출 if문 WinDecide() 함수에 포함
 
 */
--- SBSProgrammingF1/6_ 코드의 흐름 제어 과제/ChoiceAndPoint.h
+++ SBSProgrammingF1/6_ 코드의 흐름 제어 과제/ChoiceAndPoint.h
@@ -18,91 +18,126 @@ const char* computercharacter = "";
 int Action = 0; // 결과갑 도출 1: 비김, 2: 플레이어 승리, 3: 컴퓨터 승리
 //void SelectDecide()
 
-/*
 void PlayerKeyBoardSelect()
 {
-    // 기존 위치의 문자를 지우기
-    //int x = 0;
-    //int y = 4;
-    //gotoxy(x, y);
-    //printf("선택 : %s", playercharacter);
-    while (1)
-    {
+
+	while (1)
+	{
 
 
-        // 키 입력 처리
-        if (_kbhit()) {
-            int key = _getch();
-            if (key == 224) { // 화살표 키 입력은 224가 먼저 입력됨
-                key = _getch();
-                switch (key) {
-                case 72: // 위쪽 화살표
+		// 키 입력 처리
+		if (_kbhit())
+		{
+			int key = _getch();
+			if (key == 224) // 화살표 키 입력은 224가 먼저 입력됨
+			{
+				key = _getch();
+
+				if (key == 72)
+				{
 					playerchoice = 2;
-                    playercharacter = "바위";
-                    break;
-                case 80: // 아래쪽 화살표
-                    // 결과도출
-
-                    break;
-                case 75: // 왼쪽 화살표
+					playercharacter = "바위";
+					break;
+				}
+				else if (key == 75)
+				{
 					playerchoice = 1;
-                    playercharacter = "가위";
-                    break;
-                case 77: // 오른쪽 화살표
+					playercharacter = "가위";
+					break;
+				}
+				else if (key == 77)
+				{
 					playerchoice = 3;
-                    playercharacter = "보";
-                    break;
-                }
-            }
-        }
-    }
-}
-*/
-/*
-void ComputerRandomSelect()
-{
-    // 컴퓨터가 랜덤으로 선택한 결과를 저장
-	// 1. 가위 2. 바위 3. 보 (임의의 약속)
-	computerchoice = (rand() % 3) + 1; // 1~3 사이의 랜덤 숫자 생성
-    if (computerchoice == 1)
-    {
-        computercharacter = "가위";
-    }
-    else if (computerchoice == 2)
-    {
-        computercharacter = "바위";
-    }
-    else if (computerchoice == 3)
-    {
-        computercharacter = "보";
+					playercharacter = "보";
+					break;
+
+				}
+			}
+		}
+
 	}
 }
-*/
-/* void WinDecide()
+void ComputerRandomSelect()
 {
-	
-    // 플레이어와 컴퓨터의 선택을 비교해서 승패를 결정
-    if (playerchoice == computerchoice)
-    {
-        // 비김
-        
-        Action = 1;
-    }
-    else if ((playerchoice == 1 && computerchoice == 3) ||
-             (playerchoice == 2 && computerchoice == 1) ||
-             (playerchoice == 3 && computerchoice == 2))
-    {
-        // 플레이어 승리
-        playerpoint++;
-        
-        Action = 2;
-    }
-    else
-    {
-        // 컴퓨터 승리
-        computerpoint++;
+	//stand(time(NULL));  // 컴퓨터의 랜덤 출력 방식
+	// 컴퓨터가 랜덤으로 선택한 결과를 저장
+	computerchoice = (rand() % 3) + 1; // 1~3 사이의 랜덤 숫자 생성
+	if (computerchoice == 1)
+	{
+		computercharacter = "가위";
+	}
+	else if (computerchoice == 2)
+	{
+		computercharacter = "바위";
+	}
+	else if (computerchoice == 3)
+	{
+		computercharacter = "보";
+	}
+	else
+		return;
+}
+void WinDecide()
+{
+
+	// 플레이어와 컴퓨터의 선택을 비교해서 승패를 결정
+	if (playerchoice == computerchoice)
+	{
+		// 비김
+
+		Action = 1;
+	}
+	else if ((playerchoice == 1 && computerchoice == 3) ||
+		(playerchoice == 2 && computerchoice == 1) ||
+		(playerchoice == 3 && computerchoice == 2))
+	{
+		// 플레이어 승리
+		playerpoint + 1;
+
+		Action = 2;
+	}
+	else
+	{
+		// 컴퓨터 승리
+		computerpoint + 1;
 
 		Action = 3;
 	}
+
+	printf("%s\n", playercharacter);
+
+
+	printf("  ====================== \n");
+	printf("플레이어의 선택 : %s  /  컴퓨터의 선택 :  %s\n", playercharacter, computercharacter);
+
+	if (Action == 1)
+	{
+		printf("비겼습니다.\n");
+	}
+	else if (Action == 2)
+	{
+		playerpoint++;
+		printf("플레이어가 승리했습니다.\n");
+	}
+	else if (Action == 3)
+	{
+		computerpoint++;
+		printf("컴퓨터가 승리했습니다.\n");
+	}
+	printf("점수수코어 - 플레이어 %d : %d 컴퓨터\n", playerpoint, computerpoint);
+
+	printf("  ====================== \n");
+
+	if (playerpoint < 3 && computerpoint < 3)
+	{
+		printf("선택 :  ");
+	}
+	else if (playerpoint == 3)
+	{
+		printf(" --- 플레이어의 승리 ---");
+	}
+	else if (computerpoint == 3)
+	{
+		printf(" --- 컴퓨터의 승리 ---");
+	}
 }
-*/
